84b72cb5920572dce7c8e81cc685b00f68682ce3
itaniali95-byte <itaniali95@gmail.com>
2026-08-08 15:43:43 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function hello() {
     document.getElementById("message").innerHTML = "Nice to meet you! 😄";
+function showMessage() {
+    alert("Welcome to my website! 🚀");
+}
+
+function contactMe() {
+    alert("Thanks for visiting my website! 📩");
 }
